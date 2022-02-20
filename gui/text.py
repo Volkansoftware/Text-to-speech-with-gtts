9a75d5839ca5abf9c64f4b2bb63e8cf60447d011
Volkan Vural <volkanvural01@gmail.com>
2022-02-20 14:03:14 +0300

    code review
    code review

--- gui/text.py
+++ gui/text.py
@@ -1,67 +1,76 @@
-#cd gui
-#python text.py
+
 import tkinter as tk
 from tkinter import *
 import pyttsx3
-from gtts import gTTS 
-import os 
-engine=pyttsx3.init()
+from gtts import gTTS
+import os
+
+engine = pyttsx3.init()
+
+
 
 def speaknow():
-    engine.runAndWait()
-    fh = open("kitap.txt", "r")
-    myText = fh.read().replace("\n", " ")
-    language = 'tr'
-    output = gTTS(text=myText, lang=language, slow=False)
-    output.save("output.mp3")
-    fh.close()
-    os.system("start output.mp3")
-    engine.stop
-    #engine. yapman gerekebilir
-   # engine.say(textv.get())
-   #engine.runAndWait()
-    #engine.stop
+    text = e.get()
 
-root= Tk()
-
-#textv=StringVar()
-
-
-obj=LabelFrame(root,text="Text to speech",font=20,bd=1)
-obj.pack(fill="both",expand="yes",padx=10,pady=10)
-#lbl=Label(obj,text="Text",font=30)
-#lbl.pack(side=tk.LEFT,padx=5)
-
-#text=Entry(obj,textvariable=textv,font=30,width=25,bd=5)
-#text.pack(side=tk.LEFT,padx=10)
+    if text == "kitap":
+        engine.runAndWait()
+        fh = open("kitap.txt", "r")
+        myText = fh.read().replace("\n", " ")
+        language = 'tr'
+        output = gTTS(text=myText, lang=language, slow=False)
+        output.save("output.mp3")
+        fh.close()
+        os.system("start output.mp3")
+        engine.stop
+    elif text == "book":
+        engine.runAndWait()
+        fh = open("book.txt", "r")
+        myText = fh.read().replace("\n", " ")
+        language = 'en'
+        output = gTTS(text=myText, lang=language, slow=False)
+        output.save("output.mp3")
+        fh.close()
+        os.system("start output.mp3")
+        engine.stop
 
 
-btn=Button(obj,text="Speak",font=20,bg="black",fg="white",command=speaknow)
-btn.pack(side=tk.LEFT,padx=10)
+root = Tk()
+root.geometry("350x350")
+obj = LabelFrame(root, text="Text to speech", font=20, bd=1)
+obj.pack(fill="both", expand="yes", padx=10, pady=10)
+
+
+e = Entry(obj)
+
+
+
+def click(event):
+    e.config(state=NORMAL)
+    e.delete(0,END)
+
+def getBook(book,konum1,konum2):
+    name = Label(obj, text=str(book), fg='blue', font=('arial', 16))
+    name.place(x=konum1, y=konum2)
+
+
+getBook("1-) KİTAP",100,50)
+getBook("2-) BOOK",100,100)
+
+e.insert(0,"lütfen kitap ismi girin")
+e.configure(state=DISABLED)
+e.bind("<Button-1>",click)
+
+
+e.place(x=30, y=170)
+name=Label(obj,text='KİTAP İSİMLERİ', fg='red',font=('arial',16))
+name.place(x=100,y=10)
+
+
+btn = Button(obj, text="Oku", font=20, bg="black", fg="white", command=speaknow)
+btn.pack(side=tk.LEFT, padx=10)
+btn.place(x=220, y=160)
 
 root.title("Text to speech")
-root.geometry("550x250")
 
 
 root.mainloop()
-
-
-
-#from gtts import gTTS 
-  
-# ses dosyası için kullanılacak kütüphane
-#import os 
-
-#fh = open("book.txt", "r")#r read
-#myText = fh.read().replace("\n", " ")
-
-# istenilen dile çevrilme
-#language = 'tr'
-
-#output = gTTS(text=myText, lang=language, slow=False)
-
-#output.save("output.mp3")
-#fh.close()
-
-# başlatma 
-#os.system("start output.mp3")
